e1acaea0b8673e260b5bb63a4570f9b14b888fce
drcachi <doriancachim@gmail.com>
2024-10-18 18:46:39 -0400

    le cambie a livia

--- preforsa/lib/main.dart
+++ preforsa/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
 
-      home: const MyHomePage(title: 'Dorian'),
+      home: const MyHomePage(title: 'livia'),
 
     );
   }
