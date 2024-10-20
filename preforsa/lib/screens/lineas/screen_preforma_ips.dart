e26f617121c50c34d0b5d8cb53a8b434b3d48f39
jos357 <paredesjose265@gmail.com>
2024-10-20 14:33:11 -0400

    drawer, widget global

--- preforsa/lib/screens/lineas/screen_preforma_ips.dart
+++ preforsa/lib/screens/lineas/screen_preforma_ips.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preforsa/widgets/custom_drawer.dart';
 
 class ScreenPreformaIPS extends StatefulWidget {
   const ScreenPreformaIPS({super.key});
@@ -11,7 +12,6 @@ class ScreenPreformaIPS extends StatefulWidget {
 class _ScreenPreformaIPSState extends State<ScreenPreformaIPS> {
   int _selectedIndex = 0;
 
-  // Lista de los widgets para cada pantalla
   static const List<Widget> _widgetOptions = <Widget>[
     DatosScreen(),
     MateriaPrimaAditivosScreen(),
@@ -33,47 +33,49 @@ class _ScreenPreformaIPSState extends State<ScreenPreformaIPS> {
       appBar: AppBar(
         title: const Text('Preforma IPS'),
       ),
+      drawer: const CustomDrawer(
+        userName: 'Paul Perez',
+        userRole: 'Supervisor',
+      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.description), // Icono para "Datos"
+            icon: Icon(Icons.description),
             label: 'Datos',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart), // Icono para "MP y Aditivos"
+            icon: Icon(Icons.shopping_cart),
             label: 'MP y Aditivos',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search), // Icono para "Defectos"
+            icon: Icon(Icons.search),
             label: 'Defectos',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.monitor_weight), // Icono para "Pesos"
+            icon: Icon(Icons.monitor_weight),
             label: 'Pesos',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.analytics), // Icono para "Monitoreo"
+            icon: Icon(Icons.analytics),
             label: 'Monitoreo',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.thermostat), // Icono para "Temperatura"
+            icon: Icon(Icons.thermostat),
             label: 'Temperatura',
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black, // Color para el ícono seleccionado
-        unselectedItemColor:
-            Colors.black, // Color para los íconos no seleccionados
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
         onTap: _onItemTapped,
       ),
     );
   }
 }
 
-// Pantalla de "Datos"
 class DatosScreen extends StatelessWidget {
   const DatosScreen({super.key});
 
@@ -85,68 +87,44 @@ class DatosScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(14.0),
             child: GridView.count(
-              crossAxisCount: 2, // Cantidad de columnas
+              crossAxisCount: 2,
               crossAxisSpacing: 8,
               mainAxisSpacing: 8,
-              childAspectRatio:
-                  1.8, // Proporción para ajustar el tamaño de los campos
+              childAspectRatio: 1.8,
               children: <Widget>[
-                // Modalidad de Producción
                 _buildDropdownField(
                     'MODALIDAD DE PRODUCCIÓN', ['Normal', 'Otra'], 'Normal'),
-                // Lote
                 _buildTextField('LOTE'),
-                // Maquinista
                 _buildDropdownField('MAQUINISTA', ['Agustin Fernandez', 'Otro'],
                     'Agustin Fernandez'),
-                // Parte
                 _buildTextField('PARTE'),
-                // Producto
                 _buildDropdownField('PRODUCTO',
                     ['CRISTAL EC30', 'Otro Producto'], 'CRISTAL EC30'),
-                // Gramaje
                 _buildDropdownField(
                     'GRAMAJE', ['20,1 M5 R', 'Otro Gramaje'], '20,1 M5 R'),
-                // Ciclo Prom
                 _buildTextField('CICLO PROM'),
-                // Cavidades Habilitadas
                 _buildDropdownField(
                     'CAVIDADES HABILITADAS', ['96', 'Otra cantidad'], '96'),
-                // Peso Promedio
                 _buildTextField('PESO PROMEDIO'),
-                // PA Inicial
                 _buildTextField('PA INICIAL'),
-                // PA Final
                 _buildTextField('PA FINAL'),
-                // Empaque
                 _buildDropdownField('EMPAQUE', ['JAULA GRANDE', 'Otro Empaque'],
                     'JAULA GRANDE'),
-                // Cantidad
                 _buildTextField('CANTIDAD'),
-                // Peso Prom. en Cont. Neto
                 _buildTextField('PESO PROM. EN CONT. NETO'),
-                // Total de Cajas Controladas
                 _buildTextField('TOTAL DE CAJAS CONTROLADAS'),
-                // Saldos
                 _buildTextField('SALDOS'),
-                // Total de Cajas Producidas
                 _buildTextField('TOTAL DE CAJAS PRODUCIDAS'),
-                // Cantidad Total Piezas
                 _buildTextField('CANTIDAD TOTAL PIEZAS'),
-                // Cantidad Total KG
                 _buildTextField('CANTIDAD TOTAL KG'),
-                // Cantidad de Prod. Retenido
                 _buildTextField('CANTIDAD DE PROD. RETENIDO'),
-                // Conformidad Tiempo de Enfriamiento (Switch)
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text('CONFORMIDAD TIEMPO DE ENFRIAMIENTO'),
                     Switch(
                       value: false,
-                      onChanged: (bool newValue) {
-                        // Aquí puedes manejar el cambio de valor del switch
-                      },
+                      onChanged: (bool newValue) {},
                     ),
                   ],
                 ),
@@ -158,7 +136,6 @@ class DatosScreen extends StatelessWidget {
     );
   }
 
-  // Método para construir campos de texto
   Widget _buildTextField(String label) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -173,7 +150,6 @@ class DatosScreen extends StatelessWidget {
     );
   }
 
-  // Método para construir campos desplegables (Dropdown)
   Widget _buildDropdownField(
       String label, List<String> options, String defaultValue) {
     return Column(
@@ -196,112 +172,47 @@ class DatosScreen extends StatelessWidget {
   }
 }
 
-// Pantalla de "Materia Prima y Aditivos"
 class MateriaPrimaAditivosScreen extends StatelessWidget {
   const MateriaPrimaAditivosScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.all(14.0),
-          child: GridView.count(
-            crossAxisCount: 2, // Cantidad de columnas
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 8,
-            childAspectRatio: 1.8,
-            children: const <Widget>[
-              Text('Hola'),
-            ],
-          ),
-        ))
-      ],
-    );
+    return const Center(child: Text('Materia Prima y Aditivos Screen'));
   }
 }
 
-// Pantalla de "Defectos"
 class DefectosScreen extends StatelessWidget {
   const DefectosScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Defectos Screen'),
-        ElevatedButton(
-          onPressed: () {
-            // Aquí puedes agregar la funcionalidad del botón
-          },
-          child: const Text('Botón en Defectos'),
-        ),
-      ],
-    );
+    return const Center(child: Text('Defectos Screen'));
   }
 }
 
-// Pantalla de "Pesos"
 class PesosScreen extends StatelessWidget {
   const PesosScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Pesos Screen'),
-        ElevatedButton(
-          onPressed: () {
-            // Aquí puedes agregar la funcionalidad del botón
-          },
-          child: const Text('Botón en Pesos'),
-        ),
-      ],
-    );
+    return const Center(child: Text('Pesos Screen'));
   }
 }
 
-// Pantalla de "Monitoreo"
 class MonitoreoScreen extends StatelessWidget {
   const MonitoreoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Monitoreo Screen'),
-        ElevatedButton(
-          onPressed: () {
-            // Aquí puedes agregar la funcionalidad del botón
-          },
-          child: const Text('Botón en Monitoreo'),
-        ),
-      ],
-    );
+    return const Center(child: Text('Monitoreo Screen'));
   }
 }
 
-// Pantalla de "Temperatura"
 class TemperaturaScreen extends StatelessWidget {
   const TemperaturaScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Temperatura Screen'),
-        ElevatedButton(
-          onPressed: () {
-            // Aquí puedes agregar la funcionalidad del botón
-          },
-          child: const Text('Botón en Temperatura'),
-        ),
-      ],
-    );
+    return const Center(child: Text('Temperatura Screen'));
   }
 }
---
+++ preforsa/lib/widgets/custom_drawer.dart
@@ -0,0 +1,83 @@
+// custom_drawer.dart
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  final String userName;
+  final String userRole;
+
+  const CustomDrawer({
+    super.key,
+    required this.userName,
+    required this.userRole,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text(userName),
+            accountEmail: Text(userRole),
+            currentAccountPicture: CircleAvatar(
+              child: Text(
+                userName[0], // Inicial del nombre del usuario
+                style: const TextStyle(fontSize: 40.0),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.dashboard),
+            title: const Text('Opción 1'),
+            onTap: () {
+              Navigator.pop(context); // Cerrar el Drawer
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Opción 2'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Opción 3'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.help),
+            title: const Text('Opción 4'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text('Opción 5'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text('Opción 6'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Opción 7'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
