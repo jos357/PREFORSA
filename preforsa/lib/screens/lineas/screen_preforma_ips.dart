001e5262c68024850cdf18c222e68fb4bba649aa
jos357 <paredesjose265@gmail.com>
2024-10-20 20:04:06 -0400

    aqui ya esta la verificacion entre supervisor y auxiliar

--- preforsa/lib/screens/lineas/screen_preforma_ips.dart
+++ preforsa/lib/screens/lineas/screen_preforma_ips.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:preforsa/widgets/custom_drawer.dart';
 
@@ -21,6 +22,39 @@ class _ScreenPreformaIPSState extends State<ScreenPreformaIPS> {
     TemperaturaScreen(),
   ];
 
+  // Función para obtener el nombre y rol del usuario autenticado
+  Future<Map<String, String>> _getUserData() async {
+    try {
+      // Consulta a Firestore para obtener el usuario autenticado con rol "Supervisor"
+      QuerySnapshot<Map<String, dynamic>> supervisorSnapshot =
+          await FirebaseFirestore.instance
+              .collection('usuarios')
+              .where('rol', isEqualTo: 'Supervisor')
+              .get();
+
+      if (supervisorSnapshot.docs.isNotEmpty) {
+        // Si hay un supervisor autenticado, devuelve su nombre y rol
+        var data = supervisorSnapshot.docs.first.data();
+        return {
+          'userName': data['nombre'],
+          'userRole': data['rol'],
+        };
+      } else {
+        // Si no se encontró un supervisor autenticado, devuelve valores por defecto
+        return {
+          'userName': 'Ingresar',
+          'userRole': 'Auxiliar',
+        };
+      }
+    } catch (e) {
+      // En caso de error, mostrar los valores predeterminados
+      return {
+        'userName': 'Ingresar',
+        'userRole': 'Auxiliar',
+      };
+    }
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -33,9 +67,28 @@ class _ScreenPreformaIPSState extends State<ScreenPreformaIPS> {
       appBar: AppBar(
         title: const Text('Preforma IPS'),
       ),
-      drawer: const CustomDrawer(
-        userName: 'Paul Perez',
-        userRole: 'Auxiliar',
+      // Utilizamos FutureBuilder para obtener y mostrar dinámicamente los datos del usuario en el drawer
+      drawer: FutureBuilder<Map<String, String>>(
+        future: _getUserData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Muestra un loader mientras se obtienen los datos
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            // En caso de error, mostrar un Drawer con valores por defecto
+            return const CustomDrawer(
+              userName: 'Ingresar',
+              userRole: 'Auxiliar',
+            );
+          } else {
+            // Si se obtienen los datos correctamente, los mostramos en el drawer
+            var userData = snapshot.data!;
+            return CustomDrawer(
+              userName: userData['userName']!,
+              userRole: userData['userRole']!,
+            );
+          }
+        },
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
--- preforsa/lib/widgets/popup_form.dart
+++ preforsa/lib/widgets/popup_form.dart
@@ -1,12 +1,75 @@
 // lib/widgets/popup_form.dart
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PopupForm extends StatelessWidget {
   const PopupForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    Future<void> verifyPassword(
+        BuildContext context, String inputPassword) async {
+      try {
+        // Obtener todos los usuarios con el rol de Supervisor
+        QuerySnapshot<Map<String, dynamic>> supervisorSnapshot =
+            await FirebaseFirestore.instance
+                .collection('usuarios')
+                .where('rol', isEqualTo: 'supervisor')
+                .get();
+
+        bool isPasswordCorrect = false;
+        String supervisorName = '';
+
+        // Iterar sobre los supervisores para verificar la contraseña
+        for (var doc in supervisorSnapshot.docs) {
+          String firestorePassword = doc.data()['password'];
+          String userName = doc.data()['nombre'];
+
+          if (inputPassword == firestorePassword) {
+            // Si la contraseña coincide, guardar el nombre y marcar la verificación como correcta
+            isPasswordCorrect = true;
+            supervisorName = userName;
+            break; // Salir del loop si encontramos una coincidencia
+          }
+        }
+
+        if (isPasswordCorrect) {
+          // Si la verificación fue correcta, mostrar el mensaje de bienvenida
+          Navigator.of(context).pop(); // Cerrar el formulario
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Bienvenido'),
+                content: Text('Bienvenido Ingeniero $supervisorName'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Aceptar'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          // Mostrar mensaje de error si la contraseña es incorrecta
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Contraseña incorrecta')),
+          );
+        }
+      } catch (e) {
+        // Manejar errores de Firestore
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al verificar: $e')),
+        );
+      }
+    }
 
     return AlertDialog(
       title: const Text('Verificar Supervisor'),
@@ -15,7 +78,7 @@ class PopupForm extends StatelessWidget {
         children: <Widget>[
           const Text('Por favor ingrese la contraseña de supervisor'),
           TextField(
-            controller: _passwordController,
+            controller: passwordController,
             obscureText: true, // Campo de contraseña
             decoration: const InputDecoration(labelText: 'Contraseña'),
           ),
@@ -31,15 +94,8 @@ class PopupForm extends StatelessWidget {
         TextButton(
           child: const Text('Verificar'),
           onPressed: () {
-            String password = _passwordController.text;
-            if (password == '12345') {
-              Navigator.of(context)
-                  .pop(); // Cerrar si la verificación es correcta
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Contraseña incorrecta')),
-              );
-            }
+            // Llamar a la función que verifica la contraseña en Firestore
+            verifyPassword(context, passwordController.text);
           },
         ),
       ],
