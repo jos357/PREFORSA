c026be19f27ea6714701dd5afcd85ad91313aec0
drcachi <doriancachim@gmail.com>
2024-10-22 16:55:55 -0400

    hice el inicio de una pantalla principal

---
+++ preforsa/lib/screens/lineas/screen_pantalla_inicial.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class Screen_principal extends StatefulWidget {
+
+  const Screen_principal({super.key});
+
+  @override
+  State<Screen_principal> createState() => _nameState();
+}
+
+class _nameState extends State<Screen_principal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+
+
+
+
+
+    );
+  }
+}
+
+
